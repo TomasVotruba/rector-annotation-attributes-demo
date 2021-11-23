43c29bd74c3f0e7304c3fee928df428741fc6b6d
Tomas Votruba <tomas.vot@gmail.com>
2021-11-23 22:19:43 +0300

    bare setup

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Doctrine\Set\DoctrineSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Rector\Symfony\Set\SymfonySetList;
 
@@ -16,7 +17,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // A) attributes PHP 8.0
     $containerConfigurator->import(SymfonySetList::SYMFONY_52);
 
-    // B) nested with PHP 8.1 - https://symfony.com/blog/new-in-symfony-5-4-nested-validation-attributes
+    // B) doctrine attributes?
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+
+    // C) nested with PHP 8.1 - https://symfony.com/blog/new-in-symfony-5-4-nested-validation-attributes
     // $containerConfigurator->import(SymfonySetList::SYMFONY_54);
 
     // $parameters->set(Option::PHP_VERSION_FEATURES, \Rector\Core\ValueObject\PhpVersionFeature::NEW_INITIALIZERS);
--- src/ValueObject/Talk.php
+++ src/ValueObject/Talk.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\ValueObject;
 
+
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 final class Talk
 {
     /**
+     * @ORM\Column(type="string")
      * @Assert\NotNull
      */
     public $title;
 
     /**
+     * @ORM\Column(type="int")
      * @Assert\NotNull
      * @Assert\Count(min=1),
      */
