ba745824e904aea1f032ee2ba14463bdb6a23506
Tomas Votruba <tomas.vot@gmail.com>
2022-01-06 20:24:32 +0100

    most work done

--- src/ValueObject/Talk.php
+++ src/ValueObject/Talk.php
@@ -9,24 +9,15 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 final class Talk
 {
-    /**
-     * @ORM\Column(type="string")
-     * @Assert\NotNull
-     */
+    #[ORM\Column(type: 'string')]
+    #[Assert\NotNull]
     public $title;
 
-    /**
-     * @ORM\Column(type="int")
-     * @Assert\NotNull
-     * @Assert\Count(min=1),
-     */
+    #[ORM\Column(type: 'int')]
+    #[Assert\NotNull]
+    #[Assert\Count(min: 1)]
     public $userCount;
 
-    /**
-     * @Assert\All({
-     *   @Assert\NotNull(),
-     *   @Assert\Count(min=2, max=4),
-     * })
-     */
+    #[Assert\All(constraints: [new Assert\NotNull(), new Assert\Count(min: 2, max: 4)])]
     public $parts;
 }
