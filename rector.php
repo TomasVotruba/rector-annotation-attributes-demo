aee9661c330e9204c1a54c13c3fbb72c7f6ff18d
Tomas Votruba <tomas.vot@gmail.com>
2022-10-06 14:17:12 +0200

    prepare rector 0.14.5

--- rector.php
+++ rector.php
@@ -2,26 +2,27 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Rector\Symfony\Set\SymfonySetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/src'
     ]);
 
     // A) attributes PHP 8.0
-    $containerConfigurator->import(SymfonySetList::SYMFONY_52);
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_52
+    ]);
 
     // B) doctrine attributes?
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+//    $rectorConfig->sets([
+//        DoctrineSetList::DOCTRINE_ORM_29
+//    ]);
 
     // C) nested with PHP 8.1 - https://symfony.com/blog/new-in-symfony-5-4-nested-validation-attributes
-    // $containerConfigurator->import(SymfonySetList::SYMFONY_54);
-
-    // $parameters->set(Option::PHP_VERSION_FEATURES, \Rector\Core\ValueObject\PhpVersionFeature::NEW_INITIALIZERS);
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_54
+    ]);
 };
